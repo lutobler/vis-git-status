5eea738f31e36f76a251d1c8715b9f55e860ff11
Lukas Tobler <luk4s.tobler@gmail.com>
2017-09-09 22:13:58 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## vis-git-status
+
+`vis-git-status` is a  [vis](https://github.com/martanne/vis) plugin that wil display git information about the current file in the status bar (branch, has branch changed, how many commits ahead/behind).
---
+++ vis-git-status.lua
@@ -0,0 +1,98 @@
+local git_branch = '\xee\x82\xa0'
+local arrow_up = '\xe2\x86\x91'
+local arrow_down = '\xe2\x86\x93'
+
+-- get the current git branch when opening a file
+-- this needs to be done when opening a file (once), because the shell
+-- script is somewhat expensive.
+local branch_info = {}
+vis.events.subscribe(vis.events.FILE_OPEN, function(file)
+    local fname = file.name
+    local script = [[
+        pushd . > /dev/null
+        cd $(dirname "$PWD/]] .. fname .. [=[")
+        git_eng="env LANG=C git"
+        arrow_up='__ARROW_UP'
+        arrow_down='__ARROW_DOWN'
+        branch=$($git_eng symbolic-ref --short HEAD 2>/dev/null)
+        mod=$($git_eng status --porcelain)
+        modified=
+        if [[ -n "$mod" ]]; then
+            modified=" (+)"
+        fi
+        out="$branch$modified"
+        stat="$($git_eng status --porcelain --branch | grep '^##' | grep -o '\[.\+\]$')"
+        aheadN="$(echo $stat | grep -o 'ahead [[:digit:]]\+' | grep -o '[[:digit:]]\+')"
+        behindN="$(echo $stat | grep -o 'behind [[:digit:]]\+' | grep -o '[[:digit:]]\+')"
+        [[ -n "$behindN" ]] && out+=" $behindN$arrow_down"
+        [[ -n "$aheadN" ]] && out+=" $aheadN$arrow_up"
+        popd > /dev/null
+        if [[ -n "$branch" ]]; then
+            echo "__GIT_BRANCH $out"
+        fi
+    ]=]
+
+    local p = io.popen(script)
+    local r = p:read("*a")
+        :gsub("\n", "")                   -- remove trailing newline
+        :gsub("__GIT_BRANCH", git_branch) -- insert git branch unicode character
+        :gsub("__ARROW_UP", arrow_up)     -- insert arrow up unicode character
+        :gsub("__ARROW_DOWN", arrow_down) -- insert arrow down unicode character
+    p:close()
+    if r ~= "" then
+        branch_info[fname] = r
+    end
+end)
+
+-- mostly taken from vis-std.lua
+vis.events.subscribe(vis.events.WIN_STATUS, function(win)
+    local left_parts = {}
+    local right_parts = {}
+    local file = win.file
+    local selection = win.selection
+
+    local modes = {
+        [vis.modes.NORMAL] = 'NORMAL',
+        [vis.modes.OPERATOR_PENDING] = '',
+        [vis.modes.VISUAL] = 'VISUAL',
+        [vis.modes.VISUAL_LINE] = 'VISUAL-LINE',
+        [vis.modes.INSERT] = 'INSERT',
+        [vis.modes.REPLACE] = 'REPLACE',
+    }
+
+    local mode = modes[vis.mode]
+    if mode ~= '' and vis.win == win then
+        table.insert(left_parts, mode)
+    end
+
+    -- git branch
+    local branch = branch_info[file.name]
+    if branch then
+        table.insert(left_parts, branch)
+    end
+
+    table.insert(left_parts, (file.name or '[No Name]') ..
+        (file.modified and '[+]' or '') .. (vis.recording and ' @' or ''))
+
+    if #win.selections > 1 then
+        table.insert(right_parts, selection.number .. '/' .. #win.selections)
+    end
+
+    local size = file.size
+    local pos = selection.pos
+    if not pos then pos = 0 end
+    table.insert(right_parts,
+        (size == 0 and "0" or math.ceil(pos/size*100)) .. "%")
+
+    if not win.large then
+        local col = selection.col
+        table.insert(right_parts, selection.line .. ',' .. col)
+        if size > 33554432 or col > 65536 then
+            win.large = true
+        end
+    end
+
+    local left = ' ' .. table.concat(left_parts, " » ") .. ' '
+    local right = ' ' .. table.concat(right_parts, " « ") .. ' '
+    win:status(left, right)
+end)
